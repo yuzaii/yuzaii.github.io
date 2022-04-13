9b503bf51c4116e1fddcd3d0b133d08425cff4a7
zlymac <1147497022@qq.com>
2022-04-13 13:00:11 +0800

    Site updated: 2022-04-13 13:00:10

---
+++ myproject/ntuwifi/staticsource/a77.js
@@ -0,0 +1,17 @@
+/********************  浏览器探测重定向列表  ********************/
+var BrowserDetectAry = new Array(15);//0表示未使用
+BrowserDetectAry[0]="1.1.1.1";
+BrowserDetectAry[1]="www.msftncsi.com";
+BrowserDetectAry[2]="land.xiaomi.net";
+BrowserDetectAry[3]="detectportal.firefox.com";
+BrowserDetectAry[4]="www.airport.us";
+BrowserDetectAry[5]="www.thinkdifferent.us";
+BrowserDetectAry[6]="www.ibook.info";
+BrowserDetectAry[7]="www.itools.info";
+BrowserDetectAry[8]="www.appleiphonecell.com";
+BrowserDetectAry[9]="captive.apple.com";
+BrowserDetectAry[10]="www.apple.com";
+BrowserDetectAry[11]="gspe21.ls.apple.com";
+BrowserDetectAry[12]="generate_204";//安卓generate_204 返回http状态204
+BrowserDetectAry[13]="ncsi.txt";
+BrowserDetectAry[14]="success.txt";
